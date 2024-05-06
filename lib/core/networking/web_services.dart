27d7ece8451efae3ff060c745770e66d6a95eca4
Nada Rizk <63368547+Nada-gaber@users.noreply.github.com>
2024-05-06 02:02:41 +0200

    web services file

---
+++ lib/core/networking/web_services.dart
@@ -0,0 +1,13 @@
+import 'package:dio/dio.dart';
+import 'package:retrofit/http.dart';
+
+import '../constants/api_constants.dart';
+part 'web_services.g.dart';
+
+
+@RestApi(baseUrl:ApiConstants.apiUrl)
+abstract class WebServices {
+  factory WebServices(Dio dio,{String baseUrl}) = _WebServices;
+
+ }
+

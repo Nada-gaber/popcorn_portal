602ded27289634cd7a85f2116007097f6bc2496c
Nada Rizk <63368547+Nada-gaber@users.noreply.github.com>
2024-05-07 11:01:56 +0200

    created anime data model

---
+++ lib/features/home/data/model/anime_data.dart
@@ -0,0 +1,85 @@
+import 'package:json_annotation/json_annotation.dart';
+
+import 'anime_model.dart';
+
+part 'anime_data.g.dart';
+
+@JsonSerializable()
+class Data {
+  int? malId;
+  String? url;
+  Images? images;
+  Trailer? trailer;
+  bool? approved;
+  List<Titles>? titles;
+  String? title;
+  String? titleEnglish;
+  String? titleJapanese;
+  List<String>? titleSynonyms;
+  String? type;
+  String? source;
+  int? episodes;
+  String? status;
+  bool? airing;
+  Aired? aired;
+  String? duration;
+  String? rating;
+  double? score;
+  int? scoredBy;
+  int? rank;
+  int? popularity;
+  int? members;
+  int? favorites;
+  String? synopsis;
+  String? background;
+  String? season;
+  int? year;
+  Broadcast? broadcast;
+  List<Producers>? producers;
+  List<Licensors>? licensors;
+  List<Studios>? studios;
+  List<Genres>? genres;
+  List<dynamic>? explicitGenres;
+  List<Themes>? themes;
+  List<Demographics>? demographics;
+  Data({
+    this.malId,
+    this.url,
+    this.images,
+    this.trailer,
+    this.approved,
+    this.titles,
+    this.title,
+    this.titleEnglish,
+    this.titleJapanese,
+    this.titleSynonyms,
+    this.type,
+    this.source,
+    this.episodes,
+    this.status,
+    this.airing,
+    this.aired,
+    this.duration,
+    this.rating,
+    this.score,
+    this.scoredBy,
+    this.rank,
+    this.popularity,
+    this.members,
+    this.favorites,
+    this.synopsis,
+    this.background,
+    this.season,
+    this.year,
+    this.broadcast,
+    this.producers,
+    this.licensors,
+    this.studios,
+    this.genres,
+    this.explicitGenres,
+    this.themes,
+    this.demographics,
+  });
+  factory Data.fromJson(Map<String, dynamic> json) => _$DataFromJson(json);
+  Map<String, dynamic> toJson() => _$DataToJson(this);
+}

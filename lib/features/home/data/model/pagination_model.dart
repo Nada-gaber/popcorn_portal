2b25e51f6f709b41d6a270b14ad51f644713d106
Nada Rizk <63368547+Nada-gaber@users.noreply.github.com>
2024-05-08 10:48:17 +0200

    edit pagination model

--- lib/features/home/data/model/pagination_model.dart
+++ lib/features/home/data/model/pagination_model.dart
@@ -6,16 +6,17 @@ part 'pagination_model.g.dart';
 
 @JsonSerializable()
 class Pagination {
+  int? lastVisiblePage;
+  bool? hasNextPage;
+  int? currentPage;
+  Items? items;
   Pagination({
     this.lastVisiblePage,
     this.hasNextPage,
     this.currentPage,
     this.items,
   });
-  int? lastVisiblePage;
-  bool? hasNextPage;
-  int? currentPage;
-  Items? items;
+
   factory Pagination.fromJson(Map<String, dynamic> json) =>
       _$PaginationFromJson(json);
   Map<String, dynamic> toJson() => _$PaginationToJson(this);

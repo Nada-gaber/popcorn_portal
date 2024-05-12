742ca1fbc2f359e46d425d8c7235550925a0d06f
Nada Rizk <63368547+Nada-gaber@users.noreply.github.com>
2024-05-12 22:16:28 +0200

    data states

---
+++ lib/features/home/logic/cubit/anime_data_states.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+import '../../data/model/anime_model.dart';
+
+@immutable
+abstract class AnimeDataState {}
+
+class AnimeDataInitial extends AnimeDataState {}
+
+class AnimeDataLoading extends AnimeDataState {}
+
+class AnimeDataLoaded extends AnimeDataState {
+  final Anime animeData;
+
+  AnimeDataLoaded(this.animeData);
+}
+
+class AnimeDataError extends AnimeDataState {
+  final String error;
+
+  AnimeDataError(this.error);
+}

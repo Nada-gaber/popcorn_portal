4546d36f0eabfcb37296bc4cc40a86dc8f090bf2
Nada Rizk <63368547+Nada-gaber@users.noreply.github.com>
2024-05-26 09:02:43 +0200

    added hero animation to UI

---
+++ lib/features/home/ui/screens/details.dart
@@ -0,0 +1,27 @@
+import 'package:cached_network_image/cached_network_image.dart';
+import 'package:flutter/material.dart';
+
+class AnimeDetails extends StatelessWidget {
+  final String imageUrl;
+
+  const AnimeDetails({super.key, required this.imageUrl});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('hi'),
+      ),
+      body: Center(
+        child: Hero(
+          tag: imageUrl, // Same tag as in the product list
+          child: CachedNetworkImage(
+            imageUrl: imageUrl,
+            fit: BoxFit.fill,
+          ),
+        ),
+      ),
+    );
+    ;
+  }
+}

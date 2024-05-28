c91bec1f15737ebf51f55c4b74fabc79d3192dae
Nada Rizk <63368547+Nada-gaber@users.noreply.github.com>
2024-05-28 12:21:37 +0200

    anime model

--- lib/features/anime/data/model/anime.dart
+++ lib/features/anime/data/model/anime.dart
@@ -1,5 +1,6 @@
-import 'package:popcorn_portal/features/home/data/model/data_images.dart';
-import 'package:popcorn_portal/features/home/data/model/trailer_model.dart';
+// import 'package:popcorn_portal/features/anime/data/model/data_images.dart';
+
+import '../../../home/data/model/data_images.dart';
 
 class Data {
   int? malid;
@@ -11,21 +12,19 @@ class Data {
   bool? approved;
   String? duration;
   String? rating;
-  num? score;
+  // double? score;
   String? type;
   String? source;
-  num? episodes;
+  // int? episodes;
   String? status;
-  num? members;
-  num? favorites;
-  num? scoredBy;
-  num? rank;
-  num? popularity;
+  // int? members;
+  // int? favorites;
+  // int? scoredBy;
+  // int? rank;
+  // int? popularity;
   String? season;
-  num? year;
-  String? background;
-  String? synopsis;
-  Trailer? trailer;
+  // int? year;
+
   Data(
       {this.malid,
       this.url,
@@ -33,50 +32,45 @@ class Data {
       this.title,
       this.titleEnglish,
       this.titleJapanese,
-      this.approved,
-      this.duration,
+       this.approved,
+       this.duration,
       this.rating,
-      this.score,
+      // this.score,
       this.type,
       this.source,
-      this.episodes,
+      // this.episodes,
       this.status,
-      this.members,
-      this.favorites,
-      this.scoredBy,
-      this.rank,
-      this.popularity,
-      this.season,
-      this.year,
-      this.background,
-      this.synopsis});
+      // this.members,
+      // this.favorites,
+      // this.scoredBy,
+      // this.rank,
+      // this.popularity,
+      // this.season,
+      // this.year
+      });
 
   Data.fromJson(Map<String, dynamic> json) {
     malid = json['mal_id'];
     url = json['url'];
     images = json['images'] != null ? Images?.fromJson(json['images']) : null;
-    trailer =
-        json['trailer'] != null ? Trailer?.fromJson(json['trailer']) : null;
     title = json['title'];
     titleEnglish = json['titleEnglish'];
     titleJapanese = json['titleJapanese'];
     approved = json['approved'];
     duration = json['duration'];
     rating = json['rating'];
-    score = json['score'];
+    // score = json['score'];
     type = json['type'];
     source = json['source'];
-    episodes = json['episodes'];
+    // episodes = json['episodes'];
     status = json['status'];
-    members = json['members'];
-    favorites = json['favorites'];
-    scoredBy = json['scoredBy'];
-    rank = json['rank'];
-    popularity = json['popularity'];
-    season = json['season'];
-    year = json['year'];
-    background = json['background'];
-    synopsis = json['synopsis'];
+    // members = json['members'];
+    // favorites = json['favorites'];
+    // scoredBy = json['scoredBy'];
+    // rank = json['rank'];
+    // popularity = json['popularity'];
+    // season = json['season'];
+    // year = json['year'];
   }
 
   Map<String, dynamic> toJson() {
@@ -90,20 +84,18 @@ class Data {
     data['approved'] = approved;
     data['duration'] = duration;
     data['rating'] = rating;
-    data['score'] = score;
+    // data['score'] = score;
     data['type'] = type;
     data['source'] = source;
-    data['episodes'] = episodes;
+    // data['episodes'] = episodes;
     data['status'] = status;
-    data['members'] = members;
-    data['favorites'] = favorites;
-    data['scoredBy'] = scoredBy;
-    data['rank'] = rank;
-    data['popularity'] = popularity;
-    data['season'] = season;
-    data['year'] = year;
-    data['background'] = background;
-    data['synopsis'] = synopsis;
+    // data['members'] = members;
+    // data['favorites'] = favorites;
+    // data['scoredBy'] = scoredBy;
+    // data['rank'] = rank;
+    // data['popularity'] = popularity;
+    // data['season'] = season;
+    // data['year'] = year;
 
     return data;
   }

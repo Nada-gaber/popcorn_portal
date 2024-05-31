a65e7f2d6a8d557cde6642488cd6dd1a6fb10ca7
Nada Rizk <63368547+Nada-gaber@users.noreply.github.com>
2024-05-31 11:16:18 +0200

    movie web services

---
+++ lib/core/networking/movie_web_services.dart
@@ -0,0 +1,15 @@
+import 'package:dio/dio.dart';
+import 'package:retrofit/http.dart';
+
+import '../../features/home/data/model/show_model.dart';
+import '../constants/api_constants.dart';
+
+part 'movie_web_services.g.dart';
+
+@RestApi(baseUrl: ApiConstants.apiUrlMovie)
+abstract class WebServices {
+  factory WebServices(Dio dio, {String baseUrl}) = _WebServices;
+
+  @GET(ApiConstants.apiUrl)
+  Future<Show> getShowsData();
+}

b536fa522c5703c653385f9063e15c6c0088efea
Nada Rizk <63368547+Nada-gaber@users.noreply.github.com>
2024-06-17 04:41:13 +0200

    firebase done

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,13 @@
 import 'package:flutter/material.dart';
 import 'package:popcorn_portal/popcorn_portal.dart';
-
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
 import 'core/routing/app_router.dart';
 
-void main() {
+void main() async {
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(PopcornPortal(
     appRouter: AppRouter(),
   ));
